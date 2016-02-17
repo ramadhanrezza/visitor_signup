0cbd4cc75b5925bc383e14b6d4c60443b0e1ee0b
M Rezza Ramadhan <mrezza.ramadhan@yahoo.com>
2016-02-17 12:12:27 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ use App\Visitors;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 
@@ -39,6 +40,10 @@ class HomeController extends Controller {
             $visitors->phone = $data['phone'];
             $visitors->occupation = $data['occupation'];
             $visitors->save();
+
+            Mail::send('home.sendemail', array('name' => $data['name'], 'email' => $data['email'], 'phone' => $data['phone'], 'occupation' => $data['occupation']), function($message) {
+                $message->from($data['name'], $data['email'])->to('admin.email@gmail.co.id', 'Admin Name')->subject('[New Register]');
+            });
 
             return Redirect::to('/');
         }
---
+++ resources/views/home/sendemail.blade.php
@@ -0,0 +1,6 @@
+<h3>New visitor registered.</h3>
+
+<p>Name : {{ $name }}</p>
+<p>Email : {{ $email }}</p>
+<p>Phone : {{ $phone }}</p>
+<p>Occupation : {{ $occupation }}</p>
